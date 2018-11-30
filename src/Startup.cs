835a3ce6548597e4cab726d34e0b139a0fda8706
Mark McTernan <mark.mcternan@tribridge.com>
2018-11-30 02:12:14 -0500

    dependency prep for ef migration setup

--- src/Startup.cs
+++ src/Startup.cs
@@ -1,20 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+
+using auth.core.Access;
+// using auth.core.Access.Models;
 
 namespace auth.core
 {
     public class Startup
     {
+
+        public Startup(IConfiguration configuration){
+            this.Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+           // services.AddIdentity<User, IdentityRole>().AddEntityFrameworkStores<AccessContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,6 +33,10 @@ namespace auth.core
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // connect in identity
+            //app.UseAuthentication();
+
 
             app.Run(async (context) =>
             {
